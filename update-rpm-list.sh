d6c1459af307f3956754f65d216fa1d80d90bd9b
Sascha Peilicke <saschpe@gmx.de>
2013-05-29 15:47:46 +0200

    Update RPM list to SUSE-Cloud-2 media
    Also load from seperate file "rpm-list" and add an update script for it.

---
+++ update-rpm-list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Use this script to update the list of potentially installed
+# RPMs to check.
+#
+
+CLOUD_MEDIA_DIR=${1:-/srv/nfs/media/SUSE-CLOUD-2/}
+
+[[ ! -d $CLOUD_MEDIA_DIR ]] && echo "Directory doesn't exist: $CLOUD_MEDIA_DIR" && exit 1
+
+find $CLOUD_MEDIA_DIR -name *.rpm | sed 's/.*\///; s/-[0-9.+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | sort > rpm-list
