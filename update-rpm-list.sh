e1955f0fc8003931715ef3ac4dd4d3dfd51a0f9f
Sascha Peilicke <saschpe@gmx.de>
2013-11-07 14:23:25 +0100

    Rename "rpm-list" to "susecloud-rpm-list"
    Since those files are installed into /usr/lib/supportconfig/resources
    alongside other resources, it may make sense to avoid clashes.

--- update-rpm-list.sh
+++ update-rpm-list.sh
@@ -4,8 +4,8 @@
 # RPMs to check.
 #
 
-CLOUD_MEDIA_DIR=${1:-/srv/nfs/media/SUSE-CLOUD-2/}
+CLOUD_MEDIA_DIR=${1:-/srv/nfs/media/SUSE-CLOUD-3/}
 
 [[ ! -d $CLOUD_MEDIA_DIR ]] && echo "Directory doesn't exist: $CLOUD_MEDIA_DIR" && exit 1
 
-find $CLOUD_MEDIA_DIR -name *.rpm | sed 's/.*\///; s/-[0-9.+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | sort > rpm-list
+find $CLOUD_MEDIA_DIR -name *.rpm | sed 's/.*\///; s/-[0-9.+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | sort > susecloud-rpm-list
