6fb5c838e80c855680e2c19ba6d9155bac4c17ed
Thomas Hutterer <thutterer@suse.de>
2015-07-10 17:10:45 +0200

    rename files to suse-openstack-cloud*

--- update-rpm-list.sh
+++ update-rpm-list.sh
@@ -4,8 +4,8 @@
 # RPMs to check.
 #
 
-CLOUD_MEDIA_DIR=${1:-/srv/nfs/media/SUSE-CLOUD-5/}
+CLOUD_MEDIA_DIR=${1:-/srv/nfs/media/SUSE-OPENSTACK-CLOUD-6/}
 
 [[ ! -d $CLOUD_MEDIA_DIR ]] && echo "Directory doesn't exist: $CLOUD_MEDIA_DIR" && exit 1
 
-find $CLOUD_MEDIA_DIR -name *.rpm | sed 's/.*\///; s/-[0-9.+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | sort > susecloud-rpm-list
+find $CLOUD_MEDIA_DIR -name *.rpm | sed 's/.*\///; s/-[0-9.+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | sort > suse-openstack-cloud-rpm-list
